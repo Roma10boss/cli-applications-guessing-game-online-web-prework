ff0206346528be889f7ca696c5ba9ffe651b9fbb
Romario Gustave <romagustave10@gmail.com>
2019-10-22 04:21:57 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,8 +3,9 @@ def run_guessing_game(start)
 end 
 def run_guessing_game
   generator = rand (1..6)
-  print " Enter a number between 1 to 6"
+  print " Enter a number between 1 to 6 "
   guess = gets.to_i 
   if guess == generator 
-    puts " "
-  
+    puts "You guessed the correct number!"
+  elsif guess != generator 
+  puts ""
